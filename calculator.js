6c1321e050d33f0038859a7638da77762627452e
Gum Ng <jon9609@gmail.com>
2022-08-04 18:04:51 -0700

    created root route get method and respond with Hello Word

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
 // express setup
 const express = required("express");
 const app = express()
+
+// create root route to get method and send response
+app.get("/", function(req, res) {
+    res.send("Hello World");
+})
