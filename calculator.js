32926cf107c2bd4f272532e2a018e86fba4b79ab
Gum Ng <jon9609@gmail.com>
2022-08-04 18:07:44 -0700

    created server on port 3000 and log when server has started

--- calculator.js
+++ calculator.js
@@ -6,3 +6,8 @@ const app = express()
 app.get("/", function(req, res) {
     res.send("Hello World");
 })
+
+//create server on port 3000
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
