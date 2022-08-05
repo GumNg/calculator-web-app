8f47daeff74568a2c592813f4150e0d3b0176f03
Gum Ng <jon9609@gmail.com>
2022-08-05 13:24:29 -0700

    created method to handle post request to homepage

--- calculator.js
+++ calculator.js
@@ -10,6 +10,11 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
+// handles post request that comes to home route
+app.post("/", function(req, res) {
+    res.send("Thanks for posting!");
+})
+
 //create server on port 3000
 app.listen(3000, function() {
     console.log("Server started on port 3000");
