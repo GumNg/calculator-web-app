bfd30ed0a42260d4c7cd7fdfe0740442556944e2
Gum Ng <jon9609@gmail.com>
2022-08-05 13:03:06 -0700

    setup body-parser

--- calculator.js
+++ calculator.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express()
 
+// body parser setup
+const bodyParser = require("body-parser");
+
 // create root route to get method and send response
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
