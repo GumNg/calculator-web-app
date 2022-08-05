abaf1280b41dcd0ceffad59c56999add0ddda7ec
Gum Ng <jon9609@gmail.com>
2022-08-05 13:37:11 -0700

    added body-parser to use with app to grab html code posted to server

--- calculator.js
+++ calculator.js
@@ -5,6 +5,11 @@ const app = express()
 // body parser setup
 const bodyParser = require("body-parser");
 
+// express works with bodyParser
+// grabs html code posted to server
+// extended: true allows posting of nested objects
+app.use(bodyParser.urlencoded({extended: true}))
+
 // create root route to get method and send response
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
